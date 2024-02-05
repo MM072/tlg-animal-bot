3d820618144853a0bf8b5bf14b0a0db770bc726a
MM072 <159014492+MM072@users.noreply.github.com>
2024-02-05 23:29:29 +0800

    Add files via upload

--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -11,7 +11,8 @@ function sendMessage(messageObj, messageText){
 }
 
 async function handleMessage(messageObj) {
-    const messageText = messageObj.text || "";
+    console.log(messageObj)
+    const messageText = messageObj.text || " ";
 
     if(messageText.charAt(0) == "/") {
         const command = messageText.substr(1);
